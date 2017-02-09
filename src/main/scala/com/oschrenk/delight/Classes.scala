d69b08485ea79c67545af09191c0c11f950fae56
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-02-09 15:17:37 +0100

    Extract times

--- src/main/scala/com/oschrenk/delight/Classes.scala
+++ src/main/scala/com/oschrenk/delight/Classes.scala
@@ -7,14 +7,17 @@ import net.ruippeixotog.scalascraper.model.Document
 
 import java.time.LocalDate
 
-case class Class(id: Int, time: String, name: String, place: String, teacher: String, experience: String)
+case class Time(start: String, end: String)
+case class Class(id: Int, time: Time, name: String, place: String, teacher: String, experience: String)
 
 class Classes {
   def extract(document: Document, day: LocalDate) = {
     val selector = s"#accordion-${day} > tr > td"
     val cells = (document >> elementList(selector)).grouped(7)
     cells.map{ cell =>
-      val time = cell(0) >> text("p")
+      val time = (cell(0) >> text("p")).split("-") match {
+        case Array(s,e, _*) => Time(s.trim, e.trim)
+      }
       val name =cell(1) >> text("p")
       val teacher =cell(2) >> text("p")
       val experience = cell(3) >> text("p")
