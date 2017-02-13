e6f120469975fa0d0784d23b10623a0d6ad2d63d
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-02-13 15:25:24 +0100

    Organize imports

--- src/test/scala/com/oschrenk/delight/ScheduleSpec.scala
+++ src/test/scala/com/oschrenk/delight/ScheduleSpec.scala
@@ -3,7 +3,6 @@ package com.oschrenk.delight
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 
 import org.scalatest.{FlatSpec, Matchers}
-import org.scalatest.prop._
 
 import java.io.File
 import java.time.LocalDate
