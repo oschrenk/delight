f3811ae892e7e364984b8e88b7281fad5e929523
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-02-13 15:26:21 +0100

    Make variables constant

--- src/test/scala/com/oschrenk/delight/ScheduleSpec.scala
+++ src/test/scala/com/oschrenk/delight/ScheduleSpec.scala
@@ -9,17 +9,17 @@ import java.time.LocalDate
 
 class ClassesSpec extends FlatSpec with Matchers {
 
-  val doc = JsoupBrowser() .parseFile( new File(getClass.getResource("/yoga.html").toURI))
-  val day = LocalDate.of(2017, 2, 4)
+  private val Doc = JsoupBrowser() .parseFile( new File(getClass.getResource("/yoga.html").toURI))
+  private val Day = LocalDate.of(2017, 2, 4)
 
   "Classes" should "extract classes for a given day" in {
-    val classes = Schedule.extractDay(doc, day)
+    val classes = Schedule.extractDay(Doc, Day)
 
     classes.toSeq.size should be (20)
   }
 
   it should "extract classes for a whole week" in {
-    val classes = Schedule.extract(doc, day).all
+    val classes = Schedule.extract(Doc, Day).all
 
     classes.toSeq.size should be (137)
   }
