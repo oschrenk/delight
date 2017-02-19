f9b391c029aaaecdd056714f38eb149db42b456d
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-02-19 13:15:08 +0100

    Seperate Nullary from Unary cli commands

--- src/main/scala/com/oschrenk/delight/App.scala
+++ src/main/scala/com/oschrenk/delight/App.scala
@@ -3,17 +3,21 @@ package com.oschrenk.delight
 import better.files.File
 import com.typesafe.config.{Config => TypesafeConfig, ConfigFactory}
 
-sealed trait Mode
-case object NoopMode extends Mode
-case object ScheduleMode extends Mode
-case object BookMode extends Mode
-case object CancelMode extends Mode
-case object UpcomingMode extends Mode
+sealed trait CliCommand
+trait NullaryCliCommand extends CliCommand
+case object ScheduleCliCommand extends NullaryCliCommand
+case object UpcomingCliCommand extends NullaryCliCommand
+
+sealed trait UnaryCliCommand extends CliCommand {
+  def classId: Int
+}
+case class BookCliCommand(classId: Int) extends  UnaryCliCommand
+case class CancelCliCommand(classId: Int) extends  UnaryCliCommand
 
 case object Options {
-  val default = Options(NoopMode, None)
+  val default = Options(None)
 }
-case class Options(mode: Mode, classId: Option[Int])
+case class Options(command: Option[CliCommand])
 
 case class Filters(teacher: Seq[String], experience: Seq[String])
 
@@ -51,34 +55,41 @@ object DelightApp extends App {
   val parser = new scopt.OptionParser[Options]("scopt") {
     head("scopt", "0.1.0")
     cmd("schedule").text("fetch schedule for next week:\n")
-      .action( (_, c) => c.copy(mode = ScheduleMode))
+      .action( (_, c) => c.copy(command = Some(ScheduleCliCommand)))
     cmd("book").text("book class with given id")
-      .action( (_, c) => c.copy(mode = BookMode))
       .children(
         arg[Int]("<classId>")
-          .action((x, c) => c.copy(classId = Some(x)) ).text("classId")
+          .action((classId, c) => c.copy(command = Some(BookCliCommand(classId)))).text("classId")
       )
     cmd("cancel").text("cancel class with given id")
-      .action( (_, c) => c.copy(mode = CancelMode))
       .children(
         arg[Int]("<classId>")
-          .action((x, c) => c.copy(classId = Some(x)) ).text("classId")
+          .action((classId, c) => c.copy(command = Some(CancelCliCommand(classId)))).text("classId")
       )
     cmd("upcoming").text("upcoming")
-      .action( (_, c) => c.copy(mode = UpcomingMode))
+      .action( (_, c) => c.copy(command = Some(UpcomingCliCommand)))
   }
 
   parser.parse(args, Options.default) match {
     case Some(options) =>
-      options.mode match {
-        case NoopMode => println("Noop")
-        case ScheduleMode => new ScheduleCommand(Config.filters).run()
-        case BookMode =>
-          new BookCommand(SessionManager.authorize(username, password, sessionPath)).run(options.classId.get)
-        case CancelMode =>
-          new CancelCommand(SessionManager.authorize(username, password, sessionPath)).run(options.classId.get)
-        case UpcomingMode =>
-          new UpcomingCommand(SessionManager.authorize(username, password, sessionPath)).run()
+      val authorize = SessionManager.authorize(username, password, sessionPath)
+
+      options.command match {
+        case None => println("Noop")
+        case Some(cmd: NullaryCliCommand) => cmd match {
+          case ScheduleCliCommand =>
+            new ScheduleCommand(Config.filters).run()
+          case UpcomingCliCommand =>
+            new UpcomingCommand(authorize).run()
+        }
+          case Some(cmd: UnaryCliCommand) =>
+            cmd match {
+              case BookCliCommand(classId) =>
+                new CancelCommand(authorize).run(classId)
+              case CancelCliCommand(classId) =>
+                new CancelCommand(authorize).run(classId)
+            }
+
       }
     case None =>
       println("error parsing")
