8c941819e6ad74d129105ec575bba3b0e406aa70
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-02-19 13:32:11 +0100

    Move Cli parsing to own file

--- src/main/scala/com/oschrenk/delight/App.scala
+++ src/main/scala/com/oschrenk/delight/App.scala
@@ -1,46 +1,9 @@
 package com.oschrenk.delight
 
-import scopt.OptionParser
-
-sealed trait CliCommand
-trait NullaryCliCommand extends CliCommand
-case object ScheduleCliCommand extends NullaryCliCommand
-case object UpcomingCliCommand extends NullaryCliCommand
-
-sealed trait UnaryCliCommand extends CliCommand {
-  def classId: Int
-}
-case class BookCliCommand(classId: Int) extends  UnaryCliCommand
-case class CancelCliCommand(classId: Int) extends  UnaryCliCommand
-
-case object Options {
-  val default = Options(None)
-}
-case class Options(command: Option[CliCommand])
-
-
 object DelightApp extends App {
   import Config._
 
-  val parser = new OptionParser[Options]("scopt") {
-    head("scopt", "0.1.0")
-    cmd("schedule").text("fetch schedule for next week:\n")
-      .action( (_, c) => c.copy(command = Some(ScheduleCliCommand)))
-    cmd("book").text("book class with given id")
-      .children(
-        arg[Int]("<classId>")
-          .action((classId, c) => c.copy(command = Some(BookCliCommand(classId)))).text("classId")
-      )
-    cmd("cancel").text("cancel class with given id")
-      .children(
-        arg[Int]("<classId>")
-          .action((classId, c) => c.copy(command = Some(CancelCliCommand(classId)))).text("classId")
-      )
-    cmd("upcoming").text("upcoming")
-      .action( (_, c) => c.copy(command = Some(UpcomingCliCommand)))
-  }
-
-  parser.parse(args, Options.default) match {
+  Cli.parser.parse(args, Options.default) match {
     case Some(options) =>
       val authorize = SessionManager.authorize(username, password, sessionPath)
 
