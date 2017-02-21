5e25562d70aed9d3251951f11d3e09ebd64db00a
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-02-21 15:20:13 +0100

    Move things from Console to Formatters

--- src/main/scala/com/oschrenk/delight/Console.scala
+++ src/main/scala/com/oschrenk/delight/Console.scala
@@ -3,44 +3,6 @@ package com.oschrenk.delight
 import java.time.format.DateTimeFormatter
 import scala.Console.{BLUE, GREEN, MAGENTA, RED, RESET}
 
-object Console {
-  def coloredLevel(name: String, experience: Option[String]): String = {
-    experience match {
-      case Some("Beginners") => s"$RESET$GREEN$name$RESET"
-      case Some("All levels") => s"$RESET$BLUE$name$RESET"
-      case Some("Experienced") => s"$RESET$MAGENTA$name$RESET"
-      case _ => name
-    }
-  }
-
-  private val DayFormatter = DateTimeFormatter.ofPattern("EEE")
-  def pretty(c: Class): String = {
-    val id = c.id
-    val day = c.time.start.toLocalDate.format(DayFormatter)
-    val start = c.time.start.toLocalTime.toString
-    val name = coloredLevel(c.name, c.experience)
-    val teacher = c.teacher
-    val place = c.place.name
-
-    s"$id $day $start $name w/ $teacher @ $place"
-  }
-}
-object Khal {
-
-  private val DayFormatter = DateTimeFormatter.ofPattern("dd.MM.")
-  def format(c: Class): String = {
-    val day = c.time.start.toLocalDate.format(DayFormatter)
-    val start = c.time.start.toLocalTime.toString
-    val end = c.time.end.toLocalTime.toString
-    val name = c.name
-    val teacher = c.teacher
-    val p = c.place
-    val place = s""""Delight Yoga, ${p.street}, ${p.zipcode} ${p.city}, ${p.country}""""
-
-    s"khal new $day $start $end $name w/ $teacher --location $place"
-  }
-}
-
 object Formatters {
   object Class {
     def from(format: String): (Class) => String = format match {
@@ -48,25 +10,50 @@ object Formatters {
       case _ => pretty
     }
 
+    private val DayFormatter = DateTimeFormatter.ofPattern("EEE")
     val pretty: (Class) => String = (c: Class) => {
-      Console.pretty(c)
+      def coloredLevel(name: String, experience: Option[String]): String = {
+        experience match {
+          case Some("Beginners") => s"$RESET$GREEN$name$RESET"
+          case Some("All levels") => s"$RESET$BLUE$name$RESET"
+          case Some("Experienced") => s"$RESET$MAGENTA$name$RESET"
+          case _ => name
+        }
+      }
+
+      val id = c.id
+      val day = c.time.start.toLocalDate.format(DayFormatter)
+      val start = c.time.start.toLocalTime.toString
+      val name = coloredLevel(c.name, c.experience)
+      val teacher = c.teacher
+      val place = c.place.name
+
+      s"$id $day $start $name w/ $teacher @ $place"
     }
 
+    private val KhalDayFormatter = DateTimeFormatter.ofPattern("dd.MM.")
     val khal: (Class) => String = (c: Class) => {
-      Khal.format(c)
+      val day = c.time.start.toLocalDate.format(KhalDayFormatter)
+      val start = c.time.start.toLocalTime.toString
+      val end = c.time.end.toLocalTime.toString
+      val name = c.name
+      val teacher = c.teacher
+      val p = c.place
+      val place = s""""Delight Yoga, ${p.street}, ${p.zipcode} ${p.city}, ${p.country}""""
+
+      s"khal new $day $start $end $name w/ $teacher --location $place"
     }
 
     val default: (Class) => String = pretty
   }
+
   object Attendance {
-
-    private val DayFormatter = DateTimeFormatter.ofPattern("dd.MM.")
-
     private def colorClass(name: String, attended: Boolean) = {
       val color = if (attended) s"$GREEN" else s"$RED"
       s"$RESET$color$name$RESET"
     }
 
+    private val DayFormatter = DateTimeFormatter.ofPattern("dd.MM.")
     private val pretty: Attendance => String = (a: Attendance) => {
       val day = a.time.start.toLocalDate.format(DayFormatter)
       val start = a.time.start.toLocalTime.toString
