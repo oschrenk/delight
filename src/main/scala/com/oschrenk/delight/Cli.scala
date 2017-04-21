9dc343b735726783128cf7b461225a286358d81c
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-04-21 08:31:21 +0200

    Add some newlines

--- src/main/scala/com/oschrenk/delight/Cli.scala
+++ src/main/scala/com/oschrenk/delight/Cli.scala
@@ -17,11 +17,13 @@ case class Options(command: Option[CliCommand])
 object Cli {
   val parser = new OptionParser[Options]("delight") {
     head("delight", Config.version)
+
     cmd("schedule").text("fetch schedule for next week:")
       .action( (_, c) => c.copy(command = Some(ScheduleCliCommand(Formatters.Class.default))))
       .children(
         opt[String]('f', "format")
           .action((format, c) => c.copy(command = Some(ScheduleCliCommand(Formatters.Class.from(format))))))
+
     cmd("book").text("book class(es) with given id(s)")
       .children(
         arg[Int]("<classId>...").unbounded()
@@ -32,6 +34,7 @@ object Cli {
             }
             Some(BookCliCommand(priorIds :+ classId))
           })).text("classId"))
+
     cmd("cancel").text("cancel class with given id")
       .children(
         arg[Int]("<classId>...").unbounded()
@@ -42,11 +45,13 @@ object Cli {
             }
             Some(CancelCliCommand(priorIds :+ classId))
           })).text("classId"))
+
     cmd("upcoming").text("upcoming")
       .action( (_, c) => c.copy(command = Some(UpcomingCliCommand(Formatters.Class.default))))
       .children(
         opt[String]('f', "format")
           .action((format, c) => c.copy(command = Some(UpcomingCliCommand(Formatters.Class.from(format))))))
+
     cmd("previous").text("previous")
       .action( (_, c) => c.copy(command = Some(PreviousCliCommand(Formatters.Attendance.default))))
       .children(
