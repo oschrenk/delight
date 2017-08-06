bfb30f36ce96135814a3c52d11d4b809a5285239
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-08-06 16:06:02 +0200

    Move CliCommand to separate file

---
+++ src/main/scala/com/oschrenk/delight/ui/CliCommand.scala
@@ -0,0 +1,12 @@
+package com.oschrenk.delight.ui
+
+import com.oschrenk.delight.model.Attendance
+import com.oschrenk.delight.model
+
+sealed trait CliCommand
+case class ScheduleCliCommand(favorites: Boolean = false, format: model.Class => String = Formatters.Class.default) extends CliCommand
+case class UpcomingCliCommand(format: model.Class => String) extends CliCommand
+case class PreviousCliCommand(format: Attendance => String) extends CliCommand
+case object StatsCliCommand extends CliCommand
+case class BookCliCommand(classIds: Seq[Int]) extends  CliCommand
+case class CancelCliCommand(classIds: Seq[Int]) extends  CliCommand
